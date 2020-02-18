6ffed982aa66ab9378b852514630569f0977e528
jadielmwangi <jedielmwangi@gmail.com>
2020-02-18 21:29:41 +0300

    changed a form in html

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 var maleNames = ["Kwasi", "Kwandwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-var dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday"];
-function giveAkanName () {
-    var year =parseInt(document.getElementById("year").value);
-    var month=parseInt(document.getElementById("month").value);
-    var day=parseInt(document.getElementById("day").value);
-    var gender=getGender();
-    var date = new Date(year +"/" + month + "/"+day);
-    var dayBorn = date.getDay();
+var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+function giveName() {
+    var year = parseInt(document.getElementById("year").value);
+    var month = parseInt(document.getElementById("month").value);
+    var day = parseInt(document.getElementById("day").value);
+    var gender = getGender();
+    var date = new Date(year + "/" + month + "/" + day);
+    var bornDay = date.getDay();
     var akanName;
-    var validate = (year > 0 && (month >0 && month <=12) && (day > 0 && day <= 31));
+    var validate = (year > 0 && (month > 0 && month <= 12) && (day > 0 && day <= 31));
     var validateGender = (gender !== "male" && gender !== "female");
-    if (year <= 0 ) {
+    if (year <= 0) {
         alert("The year is invalid");
     }
     else if (month <= 0 || month > 12) {
         alert("The month is invalid since it's between 1 and 12");
     }
-    else if (day <= 0 || day > 31){
+    else if (day <= 0 || day > 31) {
         alert("The date is invalid");
     }
     else if (validate == false) {
         alert("Invalid Input");
     }
-    if(gender ==="male" && year > 0 && month > 0 && month < 13 && day > 0 && day < 32){
-        akanName = maleNames[dayBorn];
-    alert("You were born on "+ dayOfTheWeek[dayBorn]+ " and your Akan name is " +akanName);
+    if (gender === "male" && year > 0 && month > 0 && month < 13 && day > 0 && day < 32) {
+        akanName = maleNames[bornDay];
+        alert("You were born on " + weekDays[bornDay] + " and your Akan name is " + akanName);
     }
-    else if (gender === "female" && year > 0 && month > 0 && month < 13 && day > 0 && day < 32){
-        akanName = femaleNames[dayBorn];
-    alert("You were born on "+ dayOfTheWeek[dayBorn]+ " and your Akan name is " +akanName);
+    else if (gender === "female" && year > 0 && month > 0 && month < 13 && day > 0 && day < 32) {
+        akanName = femaleNames[bornDay];
+        alert("You were born on " + weekDays[bornDay] + " and your Akan name is " + akanName);
     }
 }
-    function getGender(){
-        var gender = document.getElementsByName("gender");
-        for(i = 0; i < gender.length; i++){
-            if(gender[i].checked){
-                return(gender[i].value)
+function getGender() {
+    var gender = document.getElementsByName("gender");
+    for (j = 0; j < gender.length; j++) {
+        if (gender[j].checked) {
+            return (gender[j].value)
         }
     }
 }
